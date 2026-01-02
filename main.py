b5c3b49907df05e8821d441f8227ffb628972fb2
Sfabi28 <samuelefabi28@gmail.com>
2026-01-02 22:54:38 +0100

    added messages (send and receive)

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import StreamingResponse
 import socket
 from io import BytesIO
 import qrcode
 from fastapi.middleware.cors import CORSMiddleware
+from typing import List
 
 app = FastAPI()
 
@@ -15,6 +16,27 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# --- GESTORE CONNESSIONI (Il "Postino") ---
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            try:
+                await connection.send_text(message)
+            except:
+                pass
+
+manager = ConnectionManager()
 
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
@@ -45,3 +67,17 @@ def connect_client():
     
     print(f"QR generato per: {url}")
     return generate_image(url)
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            print(f"Messaggio ricevuto: {data}")
+            await manager.broadcast(data)
+            
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        print("Un client si è disconnesso")
